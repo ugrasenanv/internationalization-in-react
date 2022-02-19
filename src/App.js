7d0e8357151eaf5760d0bfca7452f457c2b82823
ugrasenanv <ugrasenanv@gmail.com>
2022-02-19 23:38:53 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
-import React, {useContext} from 'react';
-import logo from './logo.svg';
+import React, { useContext } from 'react';
+// import logo from './logo192.png';
 import './App.css';
-import {FormattedMessage, FormattedDate, FormattedNumber, FormattedPlural, FormattedTime} from 'react-intl';
-import {Context} from "./components/Wrapper";
+import {
+  FormattedMessage,
+  FormattedDate,
+  FormattedNumber,
+  FormattedPlural,
+  FormattedTime,
+} from 'react-intl';
+import { Context } from './components/Wrapper';
 
 function App(props) {
   const context = useContext(Context);
@@ -11,18 +16,20 @@ function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <select value = {context.locale} onChange={context.selectLanguage}>
-          <option value= 'en'>English</option>
-          <option value= 'fr'>French</option>
-          <option value= 'ar'>Arabic</option>
-
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <select value={context.locale} onChange={context.selectLanguage}>
+          <option value="en">English</option>
+          <option value="fr">French</option>
+          <option value="ar">Arabic</option>
         </select>
         <p>
           <FormattedMessage
-              id = "app.header"
-              defaultMessage="Edit the files and save to reload"
-              values = {{fileName: 'src/App.js', code: (word)=> <strong>{word}</strong>}}
+            id="app.header"
+            defaultMessage="Edit the files and save to reload"
+            values={{
+              fileName: 'src/App.js',
+              code: (word) => <strong>{word}</strong>,
+            }}
           />
         </p>
         <a
@@ -31,48 +38,43 @@ function App(props) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FormattedMessage
-            id = "app.content"
-            defaultMessage="Learn React"
-          />
-
+          <FormattedMessage id="app.content" defaultMessage="Learn React" />
         </a>
         <FormattedMessage
-          id = "app.channel.plug"
+          id="app.channel.plug"
           defaultMessage="Tutorial brought to you by Lokalise"
-          values = {{blogName: "Lokalise"}}
+          values={{ blogName: 'Lokalise' }}
         />
-        <br/>
+        <br />
         <FormattedPlural
-            id = "app.plural"
-            defaultMessage="{amount, plural, =0 {no languages} one {# one language} few {# several languages} many {# lots of languages} other {# wrong fromat}}"
-            values = {{amount: 90}}
+          id="app.plural"
+          defaultMessage="{amount, plural, =0 {no languages} one {# one language} few {# several languages} many {# lots of languages} other {# wrong fromat}}"
+          values={{ amount: 90 }}
         />
-        <br/>
+        <br />
         <FormattedDate
-            value={props.date}
-            year = 'numeric'
-            month= 'long'
-            day = 'numeric'
-            weekday = 'long'
+          value={props.date}
+          year="numeric"
+          month="long"
+          day="numeric"
+          weekday="long"
         />
-        <br/>
+        <br />
         <FormattedNumber
-            value={20.42}
-            style="currency"
-            currencyDisplay="symbol"
-            currency="USD"
-        /><br/>
-        <FormattedNumber
-            value={10000}
+          value={20.42}
+          style="currency"
+          currencyDisplay="symbol"
+          currency="USD"
         />
-        <br/>
+        <br />
+        <FormattedNumber value={10000} />
+        <br />
         <FormattedTime
-            value={new Date()}
-            hour="numeric"
-            minute="numeric"
-            second="numeric"
-            timeZoneName="long"
+          value={new Date()}
+          hour="numeric"
+          minute="numeric"
+          second="numeric"
+          timeZoneName="long"
         />
       </header>
     </div>
@@ -80,7 +82,6 @@ function App(props) {
 }
 
 export default App;
-
 
 // import React from "react";
 // import "./style.css";
