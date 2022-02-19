d6bbc9505f1ac5e157548dafede2a1e34f8ccb92
ugrasenanv <ugrasenanv@gmail.com>
2022-02-19 23:33:47 +0530

    1 binaries uploaded

---
+++ data.js
@@ -0,0 +1,26 @@
+export default {
+  name: 'Ugra',
+  lastLogin: 'Thu Aug 04 2016 19:58:19 GMT-0700 (PDT)',
+  notifications: [
+    {
+      id: 1,
+      read: false,
+      createdAt: 1469336316043,
+    },
+    {
+      id: 2,
+      read: false,
+      createdAt: 1469336288833,
+    },
+    {
+      id: 3,
+      read: true,
+      createdAt: 1469332088829,
+    },
+    {
+      id: 4,
+      read: false,
+      createdAt: 1469332088732,
+    },
+  ],
+};
